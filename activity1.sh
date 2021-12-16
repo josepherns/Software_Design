7127da2ed9f355f2985612baa6cfa94771e470e9
LM <50027424+josepherns@users.noreply.github.com>
2021-12-16 22:27:04 +0800

    Working Docker
    Just do bash ./activity1.sh

--- activity1.sh
+++ activity1.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
 
+cp School.sqlite tempdir/.
 cp app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
+cp requirements.txt tempdir/.
 
 echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY requirements.txt /home/myapp/" >>tempdir/Dockerfile
+
 echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY app.py  /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 1010" >> tempdir/Dockerfile
+echo "COPY  app.py /home/myapp/" >> tempdir/Dockerfile
+echo "RUN pip install -r /home/myapp/requirements.txt" >> tempdir/Dockerfile
+
+echo "EXPOSE 8080" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/app.py" >> tempdir/Dockerfile
 
 cd tempdir
-docker build -t projectactivity .
-docker run -t -d -p 8080:8080 --name activity projectactivity
+docker build -t midtermproject .
+docker run -t -d -p 8080:8080 --name midterm midtermproject
 docker ps -a
